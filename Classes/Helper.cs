5a067f586af7ddb5ef35b930a0d569635449be08
Караев Сергей <karaevsv@perfectsoft.ru>
2026-04-07 23:08:30 +0300

    Форма авторизации

---
+++ Classes/Helper.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmTest.Classes
+{
+    public static class Helper
+    {
+        public static bool IsAuthorized = false;
+    }
+}
